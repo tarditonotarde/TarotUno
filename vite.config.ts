beb22647574acc5f799688ba64c87ae029ab6d12
Claudia Tardito <claudia.tardito@hertasecurity.com>
2026-01-13 17:30:14 +0100

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig(({ mode }) => ({
+  // En producción GitHub Pages necesita el nombre del repo
   base: mode === 'production' ? '/TarotUno/' : '/',
+
+  // 🔑 MUY IMPORTANTE: generar el build en /docs
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
+
   plugins: [react()],
   resolve: {
     alias: {
